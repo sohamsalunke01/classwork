7c9c7681fc965d04e2771d1acea67797741a8cf7
Sohamraje151 <74708824+Sohamraje151@users.noreply.github.com>
2021-12-22 13:21:26 +0530

    Add files via upload
    ds

---
+++ datascience/plot.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.linspace(7,50,100)
+y=x*np.linspace(100,150,100)
+plt.plot(x,y)
+print(plt.show())
---
+++ datascience/stat2.py
@@ -0,0 +1,2 @@
+import pandas as pd
+file =pd.read_csv
